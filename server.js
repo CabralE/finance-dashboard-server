cb84564426f1eebfab59039facf458975a9efa6d
ed <cabrale234@gmail.com>
2022-05-20 16:05:56 -0400

    adding currency converter API to backend

--- server.js
+++ server.js
@@ -7,6 +7,37 @@ const axios = require("axios");
 const app = express();
 app.use(cors());
 
+app.get("/convert", (req, res) => {
+  const toCurrency = req.query.to_currency;
+  const fromCurrency = req.query.from_currency;
+  console.log(toCurrency);
+  console.log(fromCurrency);
+  const options = {
+    method: "GET",
+    url: "https://alpha-vantage.p.rapidapi.com/query",
+    params: {
+      from_currency: fromCurrency,
+      function: "CURRENCY_EXCHANGE_RATE",
+      to_currency: toCurrency,
+    },
+    headers: {
+      "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(
+        response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+      );
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
 app.listen(PORT || 8000, () =>
   console.log(`Server is running on port ${PORT}`)
 );
