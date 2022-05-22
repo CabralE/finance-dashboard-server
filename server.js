a1c25e678a636433c0e510f3da6a3f5280404ced
ed <cabrale234@gmail.com>
2022-05-22 18:19:48 -0400

    adding get for currency conversion and news

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const axios = require("axios");
 const app = express();
 app.use(cors());
 
+app.get("/news", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://cnbc.p.rapidapi.com/get-meta-data",
+    headers: {
+      "X-RapidAPI-Host": "cnbc.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
 app.get("/convert", (req, res) => {
   const toCurrency = req.query.to_currency;
   const fromCurrency = req.query.from_currency;
